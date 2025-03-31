8c15a11f163828442db677bcab098127784d00e2
ninolam <lamoureux.nino@gmail.com>
2025-04-01 01:12:56 +0200

    change slider marketplace

--- src/components/marketplace/HowItWorks.tsx
+++ src/components/marketplace/HowItWorks.tsx
@@ -50,12 +50,12 @@ export default function HowItWorks() {
         {steps.map((step, index) => (
           <SwiperSlide key={index}>
             <div className="w-full rounded-lg p-10 md:p-16 bg-gradient-to-r from-[#1E1E1E] via-[#2E287A] to-[#1E1E1E] h-full">
-              <div className="max-w-3xl mx-auto text-center flex flex-col">
-                <h3 className="text-6xl md:text-8xl bricolage-grotesque text-white mb-4">{step.number}</h3>
-                <h4 className="text-xl md:text-2xl bricolage-grotesque font-medium text-white mb-6">{step.title}</h4>
+              <div className="max-w-3xl mx-auto text-center flex flex-col gap-10 h-full">
+                <h3 className="text-6xl md:text-8xl bricolage-grotesque text-white">{step.number}</h3>
+                <h4 className="text-xl md:text-2xl bricolage-grotesque font-medium text-white">{step.title}</h4>
                 <p className="text-sm md:text-base inter text-white leading-relaxed">{step.description}</p>
-                <div className="flex mt-10 justify-between">
-                {steps.map((step, index) => <div key={step.number} onClick={() => swiperRef.current?.slideTo(index)} className={`${activeSlide === index ? '!bg-white' : ''} h-1.5 w-1/4 bg-black/20 cursor-pointer`}></div>)}
+                <div className="flex mt-auto justify-between">
+                  {steps.map((step, index) => <div key={step.number} onClick={() => swiperRef.current?.slideTo(index)} className={`${activeSlide === index ? '!bg-white' : ''} h-1.5 w-1/4 bg-black/20 cursor-pointer`}></div>)}
                 </div>
               </div>
             </div>
