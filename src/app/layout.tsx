5dae2f96ea30fadb5ca50a7964d81f82eef45ef9
gilbruno <gilles_bruno@gmx.fr>
2025-04-07 22:16:04 +0200

    Add GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/common/Footer";
 import FAQ from "@/components/common/FAQ/FAQ";
 import LanguageProvider from "@/components/providers/LanguageProvider";
 import Toaster from "@/components/common/Toaster";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({
   variable: "--font-inter",
@@ -24,9 +25,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
+      <GoogleTagManager gtmId="GTM-M3W7273P" />
       <body
         className={`${inter.variable} antialiased`}
       >
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M3W7273P"
+            height="0" width="0" style={{display: "none", visibility: "hidden"}}>
+          </iframe>
+        </noscript>
+
+
         <LanguageProvider>
           <Header />
           {children}
