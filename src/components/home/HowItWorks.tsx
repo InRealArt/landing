29b612ed687ac78ea8f2635aafe6fb7fdbd4b1b9
gilbruno <gilles_bruno@gmx.fr>
2025-04-16 16:31:35 +0200

    some fixes

--- src/components/home/HowItWorks.tsx
+++ src/components/home/HowItWorks.tsx
@@ -28,10 +28,12 @@ export default function HowItWorks() {
     <section className="w-full max-w-[1600px] m-auto mt-36 px-6">
       <h1 className="text-3xl lg:text-5xl bricolage-grotesque text-center">{t('home.howItWorks.title')}</h1>
       <label className="mt-4 block text-center">{t('home.howItWorks.subtitle')}</label>
-      <div className="flex flex-col lg:flex-row gap-5 mt-10 justify-between">
+      <div className="flex flex-col lg:flex-row flex-wrap gap-5 mt-10 justify-between">
         {items.map((item, index) => (
-          <div key={index} className="flex-1 p-4 lg:p-8 border rounded-lg bg-cardBackground flex flex-col min-w-0 lg:min-w-[400px]">
-            <h1 className="text-2xl lg:text-3xl unbounded whitespace-nowrap">{item.name}</h1>
+          <div key={index} className="flex-1 p-4 lg:p-8 border rounded-lg bg-cardBackground flex flex-col min-w-0 lg:min-w-[380px] xl:min-w-[400px] lg:max-w-[32%]">
+            <h1 className="text-2xl lg:text-3xl unbounded overflow-hidden text-ellipsis">
+              {item.name}
+            </h1>
             <label className="my-4 block">{item.description}</label>
             <Button text={t('buttons.readMore')} additionalClassName="mt-auto bg-purpleColor w-full" icon={<ArrowRight />} center link={item.link} />
           </div>
