6ce75495d1fc6b6d94be08079869b5ac40304757
gilbruno <gilles_bruno@gmx.fr>
2025-04-16 15:10:47 +0200

    change wording and cards

--- src/components/home/HowItWorks.tsx
+++ src/components/home/HowItWorks.tsx
@@ -25,13 +25,13 @@ export default function HowItWorks() {
   ]
 
   return (
-    <section className="w-full max-w-90 xl:max-w-screen-xl m-auto mt-36">
+    <section className="w-full max-w-[1600px] m-auto mt-36 px-6">
       <h1 className="text-3xl lg:text-5xl bricolage-grotesque text-center">{t('home.howItWorks.title')}</h1>
       <label className="mt-4 block text-center">{t('home.howItWorks.subtitle')}</label>
-      <div className="flex flex-wrap gap-4 mt-10">
+      <div className="flex flex-col lg:flex-row gap-5 mt-10 justify-between">
         {items.map((item, index) => (
-          <div key={index} className="w-full lg:w-cardLarge p-4 lg:p-8 border rounded-lg bg-cardBackground flex flex-col">
-            <h1 className="text-2xl lg:text-3xl unbounded">{item.name}</h1>
+          <div key={index} className="flex-1 p-4 lg:p-8 border rounded-lg bg-cardBackground flex flex-col min-w-0 lg:min-w-[400px]">
+            <h1 className="text-2xl lg:text-3xl unbounded whitespace-nowrap">{item.name}</h1>
             <label className="my-4 block">{item.description}</label>
             <Button text={t('buttons.readMore')} additionalClassName="mt-auto bg-purpleColor w-full" icon={<ArrowRight />} center link={item.link} />
           </div>
