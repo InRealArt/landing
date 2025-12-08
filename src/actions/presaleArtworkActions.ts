87ae24ade424488764e260b58ff903c2a7da9395
gilbruno <gilles_bruno@gmx.fr>
2025-12-08 12:03:51 +0100

    enable order presale artworks

--- src/actions/presaleArtworkActions.ts
+++ src/actions/presaleArtworkActions.ts
@@ -133,7 +133,7 @@ export async function getPresaleArtworksByArtistId(artistId: number): Promise<Pr
                 }
             },
             orderBy: {
-                order: 'asc'
+                displayOrder: 'asc'
             }
         })
 
